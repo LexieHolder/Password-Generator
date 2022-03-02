8c4bc5d0f8622b186a2adb4eafdfe90dbac5f9e2
LexieHolder <lexieannholder@gmail.com>
2022-03-02 17:19:51 -0500

    added ability to select length of password

--- develop/script.js
+++ develop/script.js
@@ -1,7 +1,6 @@
 // Assignment code here
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+
 
 //defining variables for the password components
 var specialcharacter = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "}", "^", "_", "`", "{", "}", "|", "~"] 
@@ -9,7 +8,10 @@ var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 var lowercase = ["a","s","d","f","g","h","j","k","l","q","w","e","r","t","y","u","i","o","p","z","x","c","v","b","n","m"]
 var uppercase = ["A","S","D","F","G","H","J","K","L","Q","W","E","R","T","Y","U","I","O","P","Z","X","C","V","B","N","M"]
 
-var response;
+function generatePassword() {
+    var passwordLength = window.prompt("Please select a length for your password between 8 and 128 characters below.");
+}
+
 
 
 
@@ -20,7 +22,10 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
